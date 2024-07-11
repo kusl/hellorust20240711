21393a4aca97d9c249e9416091826c3363e8b4ea
Kushal <kushaldeveloper@gmail.com>
2024-07-11 09:28:25 -0400

    add shadowing
    from
    https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html

--- src/shadowing.rs
+++ src/shadowing.rs
@@ -6,10 +6,18 @@ mod tests {
         assert_eq!(5, x);
         let x = x + 1;
         assert_eq!(6, x);
-        { 
+        {
             let x = x * 2;
             assert_eq!(12, x);
         }
         assert_eq!(6, x);
+    }
+
+    #[test]
+    fn test_shadow_string_length() {
+        let spaces = "     ";
+        assert_eq!("    ", spaces);
+        let spaces = spaces.len();
+        assert_eq!(5, spaces);
     }
 }
