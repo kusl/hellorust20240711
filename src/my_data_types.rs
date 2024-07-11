685739b3eea9b5ca417c1ea8a516afec1414e9d6
Kushal <kushaldeveloper@gmail.com>
2024-07-11 14:40:51 -0400

    add data types
    from
    https://doc.rust-lang.org/book/ch03-02-data-types.html

--- src/my_data_types.rs
+++ src/my_data_types.rs
@@ -2,7 +2,7 @@
 mod tests {
     #[test]
     fn test_not_a_number() {
-        let guess: u32 = "42".parse().expect("Not a Number!");
+        let guess: u32 = "42".parse().expect("Avocado toast!");
         assert_eq!(42, guess);
     }
 }
